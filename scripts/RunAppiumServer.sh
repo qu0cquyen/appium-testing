026258de19742729967d4159e97f30fbff75ee41
Quyen Lu <quyen.lu@fastboy.net>
2022-09-20 20:52:29 -0400

    Changed boots up cli appium server

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -ex
 npm install -g appium@next
-appium driver install xcuitest
 appium driver install uiautomator2
 appium -v
-appium &>/dev/null &
+appium server &>/dev/null &
 
 
