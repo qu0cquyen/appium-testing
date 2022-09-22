bea0c1c320ce3c87db502d73ebc5c71e28adb2e1
Quyen Lu <quyen.lu@fastboy.net>
2022-09-22 13:24:34 -0400

    Added scripts for downloading Clover Resouces

---
+++ scripts/CloverResources.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+if [ $# -eq 2 ] 
+    then
+        
+    if curl  -J -L "$1" --output "$HOME/Desktop/clover_engine"; then
+        # Download second resouce
+        if curl -J -L "$2" --output "$HOME/Desktop/clover_launcher"; then
+            echo "Download Finished"
+        else
+            echo "Failed to download Clover Launcher"
+            exit 1
+        fi; 
+    else
+        echo "Failed to download Clover Engine"
+        exit 1 
+    fi;
+    echo "Download Complete"    
+else
+    echo "Missing arguments. This script requires 2 arguments in order to process"
+    exit 1
+fi
+
